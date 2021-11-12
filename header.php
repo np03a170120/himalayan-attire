395601495e58479060b28d8956cfee193a13d433
dreamCode <np03a170120@heraldcollege.edu.np>
2021-11-12 12:40:02 +0545

    himalayan-attire

---
+++ header.php
@@ -0,0 +1,197 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Himalayan Attire</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./fontawesome-free-5.15.2-web/css/all.css">
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body>
+    <header>
+        <div class="header-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 j-l">
+                        <div class="contact">
+                            <span class="contact-us">Contact us 24/7</span>
+                            <ul class="contact-detail">
+                                <li><a href="#"><i class="fas fa-phone-alt"></i><span>(01)- 4486712</span></a></li>
+                                <li><a href="#"><i class="fas fa-envelope"></i><span>company@email.com</span></a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-md-6 j-r">
+                        <ul class="social-icon">
+                            <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="#"><i class="fab fa-youtube"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-bottom">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-2 j-c">
+                        <div class="hb-logo">
+                            <img src="./images/logo.png" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-8 j-c j-c-c">
+                        <div class="navbar-main">
+                            <nav class="navbar navbar-expand-lg">
+                                <div class="container-fluid">
+                                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                                        aria-label="Toggle navigation">
+                                        <span class="navbar-toggler-icon"></span>
+                                    </button>
+                                    <div class="collapse navbar-collapse" id="navbarNav">
+                                        <ul class="navbar-nav">
+                                            <li class="nav-item">
+                                                <a class="nav-link active" aria-current="page" href="#">Wraps</a>
+                                                <div class="dropdown-content">
+                                                    <div class="dc-title">
+                                                        <h6>WRAPS</h6>
+                                                    </div>
+                                                    <ul class="dc-links">
+                                                        <li><a href="#">Solid Wraps </a></li>
+                                                        <li><a href="#">Ombre Pashmina Warps</a></li>
+                                                        <li><a href="#">Lace Pashmina Wraps</a></li>
+                                                        <li><a href="#">Ombre Pashmina Warps</a></li>
+                                                        <li><a href="#">Lace Pashmina Wraps</a></li>
+                                                        <li><a href="#">Printed Pashimina Wraps</a></li>
+                                                        <li><a href="#">Ombre Pashmina Warps</a></li>
+                                                        <li><a href="#">Lace Pashmina Wraps</a></li>
+                                                        <li><a href="#">Printed Pashimina Wraps</a></li>
+                                                        <li><a href="#">Solid Wraps</a></li>
+                                                        <li><a href="#">Solid Wraps </a></li>
+                                                        <li><a href="#">Ombre Pashmina Warps</a></li>
+                                                        <li><a href="#">Lace Pashmina Wraps</a></li>
+                                                    </ul>
+                                                    <div class="navbar-image">
+                                                        <img src="./images/dummy-red.png" alt="">
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">Scarfs</a>
+                                                <div class="dropdown-content">
+                                                    <div class="dc-title">
+                                                        <h6>Scarfs</h6>
+                                                    </div>
+                                                    <ul class="dc-links">
+                                                        <li><a href="#">Solid Wraps </a></li>
+                                                        <li><a href="#">Ombre Pashmina Warps</a></li>
+                                                        <li><a href="#">Lace Pashmina Wraps</a></li>
+                                                        <li><a href="#">Ombre Pashmina Warps</a></li>
+                                                        <li><a href="#">Lace Pashmina Wraps</a></li>
+                                                        <li><a href="#">Printed Pashimina Wraps</a></li>
+                                                        <li><a href="#">Ombre Pashmina Warps</a></li>
+                                                        <li><a href="#">Lace Pashmina Wraps</a></li>
+                                                        <li><a href="#">Printed Pashimina Wraps</a></li>
+                                                        <li><a href="#">Solid Wraps</a></li>
+                                                        <li><a href="#">Solid Wraps </a></li>
+                                                        <li><a href="#">Ombre Pashmina Warps</a></li>
+                                                        <li><a href="#">Lace Pashmina Wraps</a></li>
+                                                    </ul>
+                                                    <div class="navbar-image">
+                                                        <img src="./images/dummy-red.png" alt="">
+                                                    </div>
+                                                </div>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">Swals</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">Scarves</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">Male's</a>
+                                            </li>
+                                            <li class="nav-item">
+                                                <a class="nav-link" href="#">About us</a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-md-2 j-c">
+                        <!-- overlay search -->
+                        <div id="myOverlay" class="overlay">
+                            <span class="closebtn" onclick="closeSearch()" title="Close Overlay">×</span>
+                            <div class="overlay-content">
+                                <!-- <div class="oc-title">
+                                    <h4>Start Your Search</h4>
+                                </div> -->
+                                <form action="/action_page.php">
+                                    <input type="text" placeholder="Search.." name="search">
+                                    <button type="submit"><i class="fa fa-search"></i></button>
+                                </form>
+                                <div class="container">
+                                    <div class="row">
+                                        <!-- single medium sized product card -->
+                                        <div class="col-md-3">
+                                            <div class="card-mid">
+                                                <div class="cm-image">
+                                                    <img src="./images/dummy-red.png" alt="">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <ul class="header-icon">
+                            <li>
+                                <button class="openBtn" onclick="openSearch()"><i class="fas fa-search"></i></button>
+                            </li>
+                            <li>
+                                <a href="#" class="check__profile"><i class="fas fa-user-alt"></i></a>
+                            </li>
+
+                            <li><a href="#" class="check__cart"><i class="fas fa-shopping-cart"></i></a></li>
+                            <li><a href="#" class="check__wishlist"><i class="fas fa-heart"></i></a></li>
+
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </header>
+    <div id="profile" class="sidenav">
+        <div class="title-button-wrapper">
+            <div class="nav-title">
+                <h3>Account</h3>
+            </div>
+            <a class="closebtn">&times;</a>
+        </div>
+    </div>
+
+    <div id="cart" class="sidenav">
+        <div class="title-button-wrapper">
+            <h1>cart</h1>
+            <a class="closebtn">&times;</a>
+        </div>
+    </div>
+
+    <div id="wishlist" class="sidenav">
+        <div class="title-button-wrapper">
+            <h1>wishlist</h1>
+            <a class="closebtn">&times;</a>
+        </div>
+    </div>
---
+++ js/custom.js
@@ -0,0 +1,65 @@
+function openSearch() {
+    $(".myOverlay").fadeIn("slow");
+    document.getElementById("myOverlay").style.display = "block";
+    document.getElementById("myOverlay").style.animation =
+        "mynewmove 0.6s forwards";
+}
+
+function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+}
+$(".closebtn").click(function() {
+    $("#profile").removeClass("open");
+    $("#cart").removeClass("open");
+    $("#wishlist").removeClass("open");
+    $("body").removeClass("open-opacity");
+});
+
+// profile
+$(".check__profile").click(function() {
+    $("#profile").addClass("open");
+    $("body").addClass("open-opacity");
+});
+
+const $menu = $("#profile");
+$(document).mouseup((e) => {
+    if (!$menu.is(e.target) && // if the target of the click isn't the container...
+        $menu.has(e.target).length === 0
+    ) {
+        // ... nor a descendant of the container
+        $menu.removeClass("open");
+        $("body").removeClass("open-opacity");
+    }
+});
+// cart
+$(".check__cart").click(function() {
+    $("#cart").addClass("open");
+    $("body").addClass("open-opacity");
+});
+const $cart = $("#cart");
+$(document).mouseup((e) => {
+    if (!$cart.is(e.target) && // if the target of the click isn't the container...
+        $cart.has(e.target).length === 0
+    ) {
+        // ... nor a descendant of the container
+        $cart.removeClass("open");
+        $("body").removeClass("open-opacity");
+    }
+});
+
+// wishlist
+
+$(".check__wishlist").click(function() {
+    $("#wishlist").addClass("open");
+    $("body").addClass("open-opacity");
+});
+const $wishlist = $("#wishlist");
+$(document).mouseup((e) => {
+    if (!$wishlist.is(e.target) && // if the target of the click isn't the container...
+        $wishlist.has(e.target).length === 0
+    ) {
+        // ... nor a descendant of the container
+        $wishlist.removeClass("open");
+        $("body").removeClass("open-opacity");
+    }
+});
